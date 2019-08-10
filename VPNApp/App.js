ea2f7ccff3bd4c210b959aef2c06c26ceae053d0
Marcin Węglarz <marcin.weglarz01@gmail.com>
2019-08-10 20:58:44 +0200

    vpn app update

---
+++ VPNApp/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import App from "./navigation/app";
+
+export default class VpnApp extends Component {
+  render() {
+    return <App />;
+  }
+}
