463970ed8cfd7f5dd74d6f12b71dabe03d10cd4c
Marcin Węglarz <marcin.weglarz01@gmail.com>
2019-08-18 15:08:44 +0200

    Welcome view styles playing around

--- VPNApp/screens/Welcome.js
+++ VPNApp/screens/Welcome.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Button, SafeAreaView } from 'react-native'
 
 export default class Welcome extends Component {
     render() {
         return (
-            <View>
-                <Text h1> Welcome </Text>
-            </View>
+            <SafeAreaView style={styles.mainContainer}>
+                <View style={styles.middleContainer}>
+                    <Text style={styles.text}> Welcome </Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button title="GET STARTED"/>
+                </View>
+            </SafeAreaView>
         );
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    mainContainer: {
+        justifyContent: "space-between",
+        backgroundColor: "green",
+        flex: 1,
+    },
+    middleContainer: {
+        backgroundColor: "yellow",
+        paddingTop: "40%"
+    },
+    text: {
+        textAlign: "center",
+        fontSize: 30,
+        backgroundColor: "red"
+    },
+    buttonContainer: {
+        height: 100,
+        backgroundColor: "orange",
+        alignItems: "center",
+        paddingTop: 10
+    },
+})
